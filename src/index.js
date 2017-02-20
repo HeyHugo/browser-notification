28666e9bc0d2d2c13d67e18a8721180c62079d8c
Hugo Heyman <hugo.heyman@gmail.com>
2017-02-20 10:54:02 +0100

    Code formatting

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 function BrowserNotification(options) {
-
   // Handle old browsers - this way we can skip polyfill Promise and Object.assign
+  // (No browser with Notification lacks Promise or Object.assign)
   if (!window.hasOwnProperty('Notification')) {
-    console.info("This browser does not support notifications");
+    console.info('This browser does not support notifications');
 
     // Return no-op function and mock promise
     return {
       notify: () => {},
       availablePromise: {
-        then: (fn) => { fn(false); }
+        then: fn => {
+          fn(false);
+        }
       }
     };
   }
@@ -23,10 +25,9 @@ function BrowserNotification(options) {
   const availablePromise = new Promise((resolve, reject) => {
     if (Notification.permission === 'granted') {
       resolve(true);
-    }
-    else if (Notification.permission !== 'denied') {
-      Notification.requestPermission().then((permission) => {
-        if (permission === "granted") {
+    } else if (Notification.permission !== 'denied') {
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
           resolve(true);
         }
         resolve(false);
@@ -40,17 +41,17 @@ function BrowserNotification(options) {
   let cooldownActive = false;
   let focused = true;
 
-  availablePromise.then((result) => {
+  availablePromise.then(result => {
     available = result;
   });
 
   window.onfocus = () => {
     focused = true;
-  }
+  };
 
   window.onblur = () => {
     focused = false;
-  }
+  };
 
   function notify(title, notifyOptions) {
     if (!available || focused || cooldownActive) {
@@ -63,19 +64,25 @@ function BrowserNotification(options) {
       window.focus();
       settings.onClick();
       n.close();
-    }
+    };
 
     if (settings.timeout !== 0) {
-      window.setTimeout(() => {
-        n.close();
-      }, settings.timeout);
+      window.setTimeout(
+        () => {
+          n.close();
+        },
+        settings.timeout
+      );
     }
 
     if (settings.cooldown !== 0) {
       cooldownActive = true;
-      window.setTimeout(() => {
-        cooldownActive = false;
-      }, settings.cooldown);
+      window.setTimeout(
+        () => {
+          cooldownActive = false;
+        },
+        settings.cooldown
+      );
     }
   }
 
