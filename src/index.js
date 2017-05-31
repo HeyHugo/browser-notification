73a88094304d61bcbcebe3aa2272a1372b464ede
Hugo Heyman <hugo.heyman@gmail.com>
2017-05-31 20:44:26 +0200

    Simplify API and whole lib by just exporting two functions
    Instead of exporting a promise the initNotifications function now returns the ready-promise.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
-function BrowserNotification(options) {
+let settings = {
+  ignoreFocused: true,
+  timeout: 0,
+  cooldown: 0
+};
+let available = false;
+let cooldownActive = false;
+let focused = true;
+
+function initNotifications(options) {
   // Handle old browsers - this way we can skip polyfill Promise and Object.assign
-  // (No browser with Notification lacks Promise or Object.assign)
   if (!window.hasOwnProperty('Notification')) {
     console.info('This browser does not support notifications');
 
-    // Return no-op function and mock promise
+    // Mock promise always resolving to false
     return {
-      notify: () => {},
-      availablePromise: {
-        then: fn => {
-          fn(false);
-        }
-      }
+      then: fn => fn(false)
     };
   }
 
-  const defaults = {
-    ignoreFocused: true,
-    timeout: 0,
-    cooldown: 0
+  settings = Object.assign({}, settings, options);
+
+  window.onfocus = () => {
+    focused = true;
   };
-  const settings = Object.assign(defaults, options);
+
+  window.onblur = () => {
+    focused = false;
+  };
 
   const availablePromise = new Promise((resolve, reject) => {
     if (Notification.permission === 'granted') {
@@ -37,60 +43,39 @@ function BrowserNotification(options) {
     }
   });
 
-  let available = false;
-  let cooldownActive = false;
-  let focused = true;
-
   availablePromise.then(result => {
     available = result;
   });
 
-  window.onfocus = () => {
-    focused = true;
-  };
-
-  window.onblur = () => {
-    focused = false;
-  };
-
-  function notify(title, notifyOptions) {
-    if (!available || settings.ignoreFocused && focused || cooldownActive) {
-      return null;
-    }
-
-    const n = new Notification(title, notifyOptions);
-
-    n.onclick = () => {
-      window.focus();
-      n.close();
-    };
-
-    if (settings.timeout !== 0) {
-      window.setTimeout(
-        () => {
-          n.close();
-        },
-        settings.timeout
-      );
-    }
-
-    if (settings.cooldown !== 0) {
-      cooldownActive = true;
-      window.setTimeout(
-        () => {
-          cooldownActive = false;
-        },
-        settings.cooldown
-      );
-    }
-
-    return n;
-  }
-
-  return {
-    notify: notify,
-    availablePromise: availablePromise
-  };
+  return availablePromise;
 }
 
-export {BrowserNotification};
+function notify(title, notifyOptions) {
+  if (!available || (settings.ignoreFocused && focused) || cooldownActive) {
+    return null;
+  }
+
+  const notification = new Notification(title, notifyOptions);
+
+  notification.onclick = () => {
+    window.focus();
+    notification.close();
+  };
+
+  if (settings.timeout !== 0) {
+    window.setTimeout(() => {
+      notification.close();
+    }, settings.timeout);
+  }
+
+  if (settings.cooldown !== 0) {
+    cooldownActive = true;
+    window.setTimeout(() => {
+      cooldownActive = false;
+    }, settings.cooldown);
+  }
+
+  return notification;
+}
+
+export {initNotifications, notify};
